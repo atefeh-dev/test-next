1178e95b8e9eac14978643eb1e3f411f9c1b3e55
Atefeh <atefeh@Atefehs-MacBook-Air.local>
2023-12-14 17:29:44 +0330

    a

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 // store.js
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 export default store;
